54d38363a0e85610998ba72e292698c00ee71266
SparcV8 <suramerica@gmail.com>
2016-10-05 10:16:39 -0500

    Módulo #01 del procesador #01
    éste file contiene las 3 entradas del NPC y su salida.

---
+++ nPC.vhd
@@ -0,0 +1,23 @@
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+entity NPC is
+    Port ( en_NPC : in  STD_LOGIC_VECTOR (31 downto 0);
+           reset : in  STD_LOGIC;
+           clk : in  STD_LOGIC;
+           sa_NPC : out  STD_LOGIC_VECTOR (31 downto 0)
+			  );
+end NPC;
+
+architecture ARQ_NPC of NPC is
+begin
+	process (en_NPC, reset, clk)
+	begin
+		if(rising_edge(clk))then
+			if(reset = '1')then sa_NPC <= (others => '0');
+			else sa_NPC <= en_NPC;
+			end if;
+		end if;
+	end process;
+end ARQ_NPC;
+
